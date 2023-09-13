2516f83f7bfa52e247c36b1525a869d4ea8e5e97
Yeji Kim <yeji.kim.4@stonybrook.edu>
2023-09-13 17:14:13 -0400

    a commit message

--- BaseConverter.m
+++ BaseConverter.m
@@ -4,7 +4,8 @@ n = input("Enter a first base: "); % base integer
 
 % check if 2 <= n <= 10.. input is valid 
 if n <2 || n>10 
-    disp("There is an error!!")
+    disp("There is an error!!") %만약에 에러이면 어떻게
+    
 end
 
 
@@ -23,6 +24,30 @@ m = input("Enter integer");
 %new_number = dec2base(base2dec(number,n),m);
 
 %first make number(base n) --> base 10
+n_to_decimal = 0;
+for i = 1 : length(number)
+    n_to_decimal = n_to_decimal + str2num(number(i)) * (n^(length(number)-i));
+end
 
-for length
-disp(new_number)
+%disp(n_to_decimal)
+
+% 10진법을 m진법으로 바꾸기 -- output이 integer이어야되는지 물어보기
+
+m_based_num = '';
+
+while n_to_decimal > 0
+    remainder = mod(n_to_decimal,m);
+    m_based_num = append(num2str(remainder), m_based_num);
+    n_to_decimal = floor(n_to_decimal/m);
+
+
+end
+
+disp(m_based_num)
+
+
+
+
+
+
+
