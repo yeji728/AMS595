1eaa85703da839813badb3b363f98ee119eafc18
yeji728 <144481938+yeji728@users.noreply.github.com>
2023-11-21 17:48:19 -0500

    Delete C__Project1 directory

--- C__Project1/C__Project1/ddd.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  ddd.cpp
-//  C__Project1
-//
-//  Created by 김예지 on 11/14/23.
-//
-
-#include "ddd.hpp"
--- C__Project1/Problem2.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Problem2.cpp
-//  
-//
-//  Created by 김예지 on 11/18/23.
-//
-
-#include "Problem2.hpp"
