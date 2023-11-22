d7536c0583c693f1aaac129a9c5b8188ccfc0ab0
Yeji Kim <yeji.kim.4@stonybrook.edu>
2023-11-21 22:24:26 -0500

    add hw

--- C__Project1/C_project1/Q5.cpp
+++ C__Project1/C_project1/Q5.cpp
@@ -1,25 +1,68 @@
 #include <iostream>
+#include <vector>
+
+
+
+
+void print_vector(std::vector<int> v){
+
+    //std:: cout << "[";
+    for ( int i = 0; i < v.size(); ++i )  {
+
+        std:: cout << v[i];
+        
+        if (i < v.size() -1) {
+
+            std:: cout << " ";
+
+        }
+
+
+    }
+
+    //std:: cout << "]" << std:: endl;
+     std::cout << std::endl;
+}
+
+
+
+
+
 
 // Function to print Pascal's Triangle up to the nth row
 void printPascalTriangle(int n){
-     // Loop through each row
-    for (int i = 0; i < n; ++i){
-        int coef = 1;
 
-        // Loop through each element in the row
-        for ( int j = 0; j <= i; ++j){
 
-            // Print the current coefficient
-            std:: cout << coef << " ";
 
-            // Update the coefficient for the next element in the row
-            coef = coef * (i - j) / (j + 1);
+    std::vector<std::vector<int> > triangle;  // Add space between right angle brackets
 
+    for (int i = 0; i < n; ++i) {
+        std::vector<int> row(1, 1);  // Use parentheses instead of curly braces to initialize the vector
+
+        if (!triangle.empty()) {
+            // For rows after the first one
+            const std::vector<int>& lastRow = triangle.back();
+
+            for (int j = 0; j < lastRow.size() - 1; ++j) {
+                // Calculate the next element using the sum of two elements from the previous row
+                row.push_back(lastRow[j] + lastRow[j + 1]);
+            }
+
+            row.push_back(1);  // Last element of each row is always 1
         }
-        // Move to the next line for the next row
-        std:: cout << std:: endl;
+
+        triangle.push_back(row);
+
+        // Print each row using the print_vector function
+        print_vector(row);
     }
 }
+
+
+
+
+
+
 
 int main(){
 
